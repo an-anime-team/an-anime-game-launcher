d4698f11fa97329ee9af8aff8736ad343625342f
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2021-10-13 17:18:54 +0200

    Added semicolons for the require statements in entry file

--- entry.js
+++ entry.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow ()
 {
