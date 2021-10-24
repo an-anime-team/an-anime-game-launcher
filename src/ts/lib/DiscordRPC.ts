088703b86093799ea0f68b96214cf3c61108f156
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2021-10-24 20:52:04 +0200

    Removed excess code

--- src/ts/lib/DiscordRPC.ts
+++ src/ts/lib/DiscordRPC.ts
@@ -25,9 +25,7 @@ export class DiscordRPC
     {
         this.rpc.setActivity({
             ...activity,
-            ...{
-                instance: false
-            }
+            instance: false
         });
     }
 
