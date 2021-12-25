c2b8623718a1f7e4a82a738aa4eda1ff1bdaa498
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2021-12-25 17:44:27 +0200

    Several changes - added components system - made settings template settings - added `vue-i18n`, made template locales - made `Checkbox` component

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,12 +1,38 @@
 <template>
-    <h1>{{ property }}</h1>
+    <div class="checkbox" :class="{'checkbox-active': active}">
+        {{ $t(`settings.${locale.split('.')[0]}.items.${locale.split('.')[1]}`) }}
+
+        <div class="checkbox-mark" @click="updateSetting">
+            <img src="../assets/svgs/checkmark.svg" />
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Configs from '../ts/Configs';
 
 export default defineComponent({
-    props: ['property'],
+    props: ['locale', 'prop'],
 
+    data: () => ({
+        active: false
+    }),
+
+    async created()
+    {
+        this.active = await Configs.get(this.prop) as boolean;
+    },
+
+    methods: {
+        updateSetting()
+        {
+            this.active = !this.active;
+
+            Configs.set(this.prop, this.active);
+        }
+    }
 });
 </script>
+
+<style src="../sass/components/checkbox.sass" lang="sass" scoped></style>
