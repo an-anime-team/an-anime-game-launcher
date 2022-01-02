cd3f2e5346d161ac3405dc48ad53108173373d81
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2022-01-02 02:03:18 +0200

    Added AppImage bundler

---
+++ bundle-appimage.cjs
@@ -0,0 +1,42 @@
+const path = require('path');
+
+// Require bundler
+const { Bundler } = require('neutralino-appimage-bundler');
+
+// Create an object with some params
+const bundler = new Bundler({
+    // .desktop file properties
+    desktop: {
+        // Name field
+        name: 'An Anime Game Launcher',
+
+        // Path to the icon
+        icon: path.join(__dirname, 'public/icons/64x64.png'),
+
+        // Categories (defult is Utilities)
+        categories: ['Game']
+    },
+
+    // Neutralino binary info
+    binary: {
+        // Name of the binary (cli.binaryName)
+        name: 'an-anime-game-launcher',
+
+        // Dist folder path
+        dist: path.join(__dirname, 'dist')
+    },
+
+    // Some files or folders to copy inside of the the AppImage
+    copy: {
+        'public': path.join(__dirname, 'dist/an-anime-game-launcher/public')
+    },
+
+    // Path to the appimage to save
+    output: path.join(__dirname, 'dist/An Anime Game Launcher.AppImage'),
+
+    // Application version
+    version: '2.0.0'
+});
+
+// Bundle project
+bundler.bundle();
