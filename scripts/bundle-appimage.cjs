147aed82dfe3f360f284629b468fd4a54aba3a59
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2022-01-31 22:43:36 +0200

    2.1.1 - fixed patch applying issue

--- scripts/bundle-appimage.cjs
+++ scripts/bundle-appimage.cjs
@@ -39,7 +39,7 @@ const bundler = new Bundler({
     output: path.join(__dirname, '../dist/An Anime Game Launcher.AppImage'),
 
     // Application version
-    version: '2.1.0'
+    version: '2.1.1'
 });
 
 // Bundle project
