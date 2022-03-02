6c5bcd7677d62246ec7b51f6a611d6f8faa8f8a7
Potato1682 <ptt1682@gmail.com>
2022-03-02 12:41:45 +0000

    Add Japanese translation

--- src/ts/launcher/Locales.ts
+++ src/ts/launcher/Locales.ts
@@ -11,7 +11,7 @@ type AvailableLocales =
     // Supported by the game's API
     | 'en-us' | 'ru-ru' | 'es-es'
     | 'de-de' | 'fr-fr' | 'id-id'
-    | 'zh-cn'
+    | 'ja-jp' | 'zh-cn'
 
     // Unsupported by the game's API
     | 'it-it' | 'hu-hu' | 'uwu'
@@ -25,7 +25,7 @@ export default class Locales
      * List of locales supported by the game's API
      */
     public static readonly supportedLocales: AvailableLocales[] = [
-        'en-us', 'ru-ru', 'es-es', 'de-de', 'fr-fr', 'id-id', 'zh-cn'
+        'en-us', 'ru-ru', 'es-es', 'de-de', 'fr-fr', 'id-id', 'ja-jp', 'zh-cn'
     ];
 
     /**
