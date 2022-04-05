96ec7d206af018edafa75a1e69e56ee3caf8a38a
Maroxy <marie@kaifa.ch>
2022-04-05 14:48:56 +0200

    Svelte Gallery package added Created Image component Add screenshots to vite config Create base of Screenshots window

---
+++ src/components/Image.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let src: string = '';
+    export let width: number = 0;
+    export let height: number = 0;
+    export let orginialURI: string = '';
+
+    export let click: () => void = () => {};
+</script>
+
+<!-- svelte-ignore a11y-img-redundant-alt -->
+<img
+    src={src}
+    data-originalURI={orginialURI}
+    width={width}
+    height={height}
+    alt="no image"
+    on:click={click}
+/>
--- src/screenshots.svelte
+++ src/screenshots.svelte
@@ -5,7 +5,22 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    import { Configs, Windows } from './empathize';
+    import { Configs, Windows, path } from './empathize';
+
+    import Gallery from 'svelte-gallery';
+    import Image from './components/Image.svelte';
+
+    import constants from './ts/Constants';
+
+    function encodeBase64Bytes(bytes: Uint8Array): string {
+        return btoa(
+            bytes.reduce((acc, current) => acc + String.fromCharCode(current), "")
+        );
+    }
+
+    const images = [
+        { src: 'https://source.unsplash.com/random', width: 600, height: 400 }
+    ];
 
     onMount(async () => {
         await Windows.current.show();
@@ -18,5 +33,25 @@
 
             document.body.setAttribute('data-theme', theme as string);
         });
+
+        Neutralino.filesystem.readBinaryFile(await constants.paths.gameDir + '/ScreenShot/20220317130153.png').then(async img => {
+            let uintdata = new Uint8Array(img);
+            images.push({
+                src: 'data:image/png;base64,' + encodeBase64Bytes(uintdata),
+                // @ts-expect-error
+                orginialURI: await constants.paths.gameDir + '/ScreenShot/20220317130153.png',
+                width: 600,
+                height: 400,
+                click: async (e) => {
+                    Neutralino.os.execCommand(`xdg-open "${path.addSlashes(e.target.getAttribute('data-originalURI'))}"`, {
+                        background: true
+                    });
+                }
+            });
+        });
     });
 </script>
+
+<main>
+    <Gallery imageComponent={Image} {images} />
+</main>
