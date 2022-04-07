0dceb1dc527a3fe4b65886b0562c312231fc394d
Maroxy <marie@kaifa.ch>
2022-04-07 22:46:21 +0200

    Fix typo in Image component

--- src/components/Image.svelte
+++ src/components/Image.svelte
@@ -2,7 +2,7 @@
     export let src: string = '';
     export let width: number = 0;
     export let height: number = 0;
-    export let orginialURI: string = '';
+    export let orignialURI: string = '';
 
     export let click: () => void = () => {};
 </script>
@@ -10,7 +10,7 @@
 <!-- svelte-ignore a11y-img-redundant-alt -->
 <img
     src={src}
-    data-originalURI={orginialURI}
+    data-originalURI={orignialURI}
     width={width}
     height={height}
     alt="no image"
