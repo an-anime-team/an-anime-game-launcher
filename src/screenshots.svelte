c97b10fbf1f9aa86c8d1a895ac229b83e183abad
Maroxy <marie@kaifa.ch>
2022-04-09 22:32:51 +0200

    Screenshots page now fully works

--- src/screenshots.svelte
+++ src/screenshots.svelte
@@ -4,11 +4,13 @@
 
 <script lang="ts">
     import { onMount } from 'svelte';
+    import { _, locale } from 'svelte-i18n';
 
     import { Configs, Windows, path } from './empathize';
 
     import Gallery from 'svelte-gallery';
     import Image from './components/Image.svelte';
+    import Button from './components/Button.svelte';
 
     import constants from './ts/Constants';
 
@@ -24,6 +26,9 @@
     };
 
     const images = new Array();
+
+    // Svelte did a haha funny and didn't want to fucking bother working with the images array so use this as an jank alternative.
+    let arrayempty = true;
 
     onMount(async () => {
         // This exists as you can't make an for loop asynchronus causing the window to load before every image is loaded into the HTML content.
@@ -41,14 +46,20 @@
                     const f2time = new Date(f2date).getTime();
                     return f2time - f1time;
                 });
+            
+            if (sortedimgs.length > 0) {
+                // Only let 4 items be in the array by modifying the length which is quicker than splicing.
+                sortedimgs.length = 4;
+                arrayempty = false;
+            }
 
-            for (let index = 0, len = 4; index < len && !(index > sortedimgs.length); ++index) {
-                Neutralino.filesystem.readBinaryFile(await constants.paths.gameDir + '/ScreenShot/' + sortedimgs[index].entry).then(async img => {
+            sortedimgs.forEach(async img => {
+                Neutralino.filesystem.readBinaryFile(await constants.paths.gameDir + '/ScreenShot/' + img.entry).then(async img => {
                     let uintdata = new Uint8Array(img);
                     
                     images.push({
                         src: 'data:image/png;base64,' + encodeBase64Bytes(uintdata),
-                        orignialURI: await constants.paths.gameDir + '/ScreenShot/' + sortedimgs[index].entry,
+                        orignialURI: await constants.paths.gameDir + '/ScreenShot/' + img.entry,
                         width: 480,
                         height: 270,
                         click: async (e) => {
@@ -58,7 +69,7 @@
                         }
                     });
                 });
-            }
+            });
 
             await fakewait();
 
@@ -76,8 +87,28 @@
     });
 </script>
 
-<main>
-    <div style="width: 95%; padding-left: 2.4%; padding-top: 1.5%;">
-        <Gallery imageComponent={Image} gutter={6} {images} />
-    </div>
-</main>
+{#if typeof $locale === 'string'}
+    <main>
+        <h2 class="centtext">{$_('screenshots.heading')}</h2>
+
+        <p class="centtext">{$_('screenshots.info.0')}</p>
+        <p class="centtext">{$_('screenshots.info.1')}</p>
+
+        {#if !arrayempty}
+            <div class="galleryholder">
+                <Gallery imageComponent={Image} gutter={6} {images} />
+            </div>
+        {:else}
+            <p class="centtext">{$_('screenshots.noimages')}</p>
+        {/if}
+
+        <div class="buttons">
+            <!-- svelte-ignore missing-declaration -->
+            <Button lang="screenshots.button" click={async () => {
+                Neutralino.os.execCommand(`xdg-open "${path.addSlashes(await constants.paths.gameDir + '/ScreenShot/')}"`, {
+                    background: true
+                });
+            }} />
+        </div>
+    </main>
+{/if}
