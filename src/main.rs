724ff90329972589c28ceb7f9632f89cf2061588
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2022-08-02 23:44:33 +0200

    Changed app's id

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ pub mod lib;
 
 use ui::*;
 
-pub const APP_ID: &str = "com.gitlab.an-anime-team.an-anime-game-launcher-gtk";
+pub const APP_ID: &str = "moe.launcher.an-anime-game-launcher-gtk";
 pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
 pub const APP_DEBUG: bool = cfg!(debug_assertions);
 
