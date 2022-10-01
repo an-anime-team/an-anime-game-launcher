744126899894b588c4ee4289ad6b0abb10b0d593
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2022-10-01 18:40:18 +0200

    2.4.0 From previous commits: - Improved failes integrity checking   also now launcher will try to repair files that were unnable to be patched via hdiff - progress bar text visibility improvement (#16)

--- scripts/bundle-appimage.cjs
+++ scripts/bundle-appimage.cjs
@@ -36,10 +36,10 @@ const bundler = new Bundler({
     },
 
     // Path to the appimage to save
-    output: path.join(__dirname, '../dist/an-anime-game-launcher-2.3.9.AppImage'),
+    output: path.join(__dirname, '../dist/an-anime-game-launcher-2.4.0.AppImage'),
 
     // Application version
-    version: '2.3.9'
+    version: '2.4.0'
 });
 
 // Bundle project
