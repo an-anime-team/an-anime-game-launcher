96645369186a9bb8a018cf2975191e4548e72fba
wangyilei.lk <wangyilei.lk@bytedance.com>
2022-10-15 23:48:48 +0800

    format: HDiffPatch.patch variable name

--- src/ts/core/HDiffPatch.ts
+++ src/ts/core/HDiffPatch.ts
@@ -8,16 +8,16 @@ export default class HDiffPatch
     public static patch(file: string, patch: string, output: string): Promise<boolean>
     {
         return new Promise(async (resolve) => {
-            let result = await Neutralino.os.execCommand(`./public/hdiffpatch/hpatchz -f "${path.addSlashes(file)}" "${path.addSlashes(patch)}" "${path.addSlashes(output)}"`);
+            let exeResult = await Neutralino.os.execCommand(`./public/hdiffpatch/hpatchz -f "${path.addSlashes(file)}" "${path.addSlashes(patch)}" "${path.addSlashes(output)}"`);
 
-            const ret = (result.stdOut ?? result.stdErr).includes('patch ok!');
+            const result = (exeResult.stdOut ?? exeResult.stdErr).includes('patch ok!');
 
             Debug.log({
                 function: 'HDiffPatch.patch',
-                message: { file, patch, output, ret }
+                message: { file, patch, output, result }
             });
 
-            resolve(ret);
+            resolve(result);
         });
     }
 };
