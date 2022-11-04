1e08c90add329921695faee7eb5666ba04b227d0
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2022-11-04 19:08:30 +0200

    2.4.1 - optimized experience on SteamOS (Steam Deck) (#21) - added Turkish - swap default virtual desktop resolution dimensions (#27) - fixed voiceovers updating - removed analytics window - changed wine sync environment variables - updated runners list

--- scripts/bundle-appimage.cjs
+++ scripts/bundle-appimage.cjs
@@ -36,10 +36,10 @@ const bundler = new Bundler({
     },
 
     // Path to the appimage to save
-    output: path.join(__dirname, '../dist/an-anime-game-launcher-2.4.0.AppImage'),
+    output: path.join(__dirname, '../dist/an-anime-game-launcher-2.4.1.AppImage'),
 
     // Application version
-    version: '2.4.0'
+    version: '2.4.1'
 });
 
 // Bundle project
