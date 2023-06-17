cdef7c115aa5cf30618777a7988cbb47e2077fe5
Observer KRypt0n_ <suimin.tu.mu.ga.mi@gmail.com>
2023-06-17 12:31:04 +0200

    revert: returned back old background picture path

--- src/main.rs
+++ src/main.rs
@@ -53,7 +53,7 @@ lazy_static::lazy_static! {
     pub static ref DEBUG_FILE: PathBuf = LAUNCHER_FOLDER.join("debug.log");
 
     /// Path to `background` file. Standard is `$HOME/.cache/anime-game-launcher/background`
-    pub static ref BACKGROUND_FILE: PathBuf = CACHE_FOLDER.join("background");
+    pub static ref BACKGROUND_FILE: PathBuf = LAUNCHER_FOLDER.join("background");
 
     /// Path to `.keep-background` file. Used to mark launcher that it shouldn't update background picture
     /// 
