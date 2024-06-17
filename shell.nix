e131dab8d2678bbb3ed40f5fb7e35e0018a7f6f4
Nikita Podvirnyi <krypt0nn@vk.com>
2024-06-17 11:01:49 +0200

    build: updated shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 let
     nixpkgs = builtins.fetchGit {
-        name = "nixos-23.11";
+        name = "nixos-24.05";
         url = "https://github.com/nixos/nixpkgs";
-        ref = "refs/heads/nixos-23.11";
+        ref = "refs/heads/nixos-24.05";
     };
 
     nixpkgs-unstable = builtins.fetchGit {
@@ -23,6 +23,8 @@ in pkgs.mkShell {
         gcc
         cmake
         pkg-config
+
+        xdelta
     ];
 
     buildInputs = with pkgs; [
