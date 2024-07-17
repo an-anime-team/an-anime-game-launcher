14c71beb0965b44c8019afff7e46d2498fceaa53
ezKEa <wrp7bapuk@mozmail.com>
2024-07-17 07:14:55 -0400

    build: replace shell.nix with flake.nix for dev shell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.x86_64-linux;
+  in {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      nativeBuildInputs = with pkgs; [
+          pkgs-unstable.rustup
+          pkgs-unstable.rustfmt
+          pkgs-unstable.clippy
+
+          gcc
+          cmake
+          pkg-config
+
+          xdelta
+          libwebp
+      ];
+
+      buildInputs = with pkgs; [
+          gtk4
+          glib
+          gdk-pixbuf
+          gobject-introspection
+
+          libadwaita
+      ];
+    };
+  };
+}
